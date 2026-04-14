127bc8c2aa557f55b65cd842c1bba1a4ba582574
vihangamanusha <vihangamanusha07@gmail.com>
2026-04-14 07:10:23 +0530

    update html name

--- src/main/java/com/fot/eventsystem/controller/AdminController.java
+++ src/main/java/com/fot/eventsystem/controller/AdminController.java
@@ -1,4 +1,15 @@
 package com.fot.eventsystem.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "admin/dashboard";
+    }
 }
