3c20f86256ea8110276505fd20507bd01ee0b0af
vihangamanusha <vihangamanusha07@gmail.com>
2026-04-22 19:54:50 +0530

    add member and public controller

--- src/main/java/com/fot/eventsystem/controller/MemberController.java
+++ src/main/java/com/fot/eventsystem/controller/MemberController.java
@@ -1,4 +1,13 @@
 package com.fot.eventsystem.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MemberController {
+
+    @GetMapping("/member/home")
+    public String memberHome() {
+        return "member/home"; // member/home.html
+    }
 }
--- src/main/java/com/fot/eventsystem/controller/PublicController.java
+++ src/main/java/com/fot/eventsystem/controller/PublicController.java
@@ -1,4 +1,13 @@
 package com.fot.eventsystem.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PublicController {
+
+    @GetMapping("/public/home")
+    public String publicHome() {
+        return "public/home"; // public/home.html
+    }
 }
