26be9184901acc4bd8124acb711de6fe2d723ac5
vihangamanusha <vihangamanusha07@gmail.com>
2026-04-24 15:06:23 +0530

    update membercontroller

--- src/main/java/com/fot/eventsystem/controller/MemberController.java
+++ src/main/java/com/fot/eventsystem/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.fot.eventsystem.controller;
 
 import com.fot.eventsystem.model.User;
+import com.fot.eventsystem.repository.VenueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import jakarta.servlet.http.HttpSession;
@@ -8,7 +10,9 @@ import org.springframework.ui.Model;
 
 @Controller
 public class MemberController {
-
+    @Autowired
+    private VenueRepository venueRepository;
+    
     @GetMapping("/member/home")
     public String memberHome(HttpSession session, Model model) {
 
@@ -24,7 +28,7 @@ public class MemberController {
         }
 
         model.addAttribute("user", user);
-
+        model.addAttribute("venues", venueRepository.findAll());
         return "member/home";
     }
 }
