791426c762d151003aed4934659f67ddd45faa74
vihangamanusha <vihangamanusha07@gmail.com>
2026-04-25 15:21:54 +0530

    update

--- src/main/java/com/fot/eventsystem/controller/AdminController.java
+++ src/main/java/com/fot/eventsystem/controller/AdminController.java
@@ -1,11 +1,14 @@
 package com.fot.eventsystem.controller;
 
+import com.fot.eventsystem.model.Booking;
 import org.springframework.ui.Model;
 import com.fot.eventsystem.repository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -38,5 +41,16 @@ public class AdminController {
         model.addAttribute("bookings", bookingRepository.findAll());
 
         return "admin/booking-request";
+    }
+
+    @GetMapping("/approved-events")
+    public String approvedEvents(Model model) {
+
+        List<Booking> approvedList =
+                bookingRepository.findByStatusIgnoreCase("APPROVED");
+
+        model.addAttribute("bookings", approvedList);
+
+        return "admin/approved-events";
     }
 }
--- src/main/java/com/fot/eventsystem/repository/BookingRepository.java
+++ src/main/java/com/fot/eventsystem/repository/BookingRepository.java
@@ -3,9 +3,11 @@ package com.fot.eventsystem.repository;
 import com.fot.eventsystem.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
         int countByStatus(String status);
-
+        List<Booking> findByStatusIgnoreCase(String status);
 
 }
