19838cca4a7774e4ce51cf99706e2d3b4f8a8299
vihangamanusha <vihangamanusha07@gmail.com>
2026-04-25 19:57:35 +0530

    update

--- src/main/java/com/fot/eventsystem/controller/HomeController.java
+++ src/main/java/com/fot/eventsystem/controller/HomeController.java
@@ -1,11 +1,19 @@
 package com.fot.eventsystem.controller;
 
+import com.fot.eventsystem.model.Booking;
+import com.fot.eventsystem.repository.BookingRepository;
 import com.fot.eventsystem.repository.NewsRepository;
 import com.fot.eventsystem.repository.VenueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -15,6 +23,9 @@ public class HomeController {
 
     @Autowired
     private NewsRepository newsRepository;
+
+    @Autowired
+    private BookingRepository bookingRepository;
 
     @GetMapping("/")
     public String home(Model model) {
@@ -27,6 +38,11 @@ public class HomeController {
     public String aboutPage(Model model) {
         model.addAttribute("venues", venueRepository.findAll());
         return "about";
+    }
+
+    @GetMapping("/calendar")
+    public String calendar() {
+        return "calendar";
     }
 
 
